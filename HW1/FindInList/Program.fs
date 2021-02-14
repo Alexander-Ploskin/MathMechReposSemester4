d33b6eac2fba5198c15063202e478a1b7124c834
Alexander-Ploskin <ploskin0107@gmail.com>
2021-02-14 17:53:41 +0300

    made searching

---
+++ HW1/FindInList/Program.fs
@@ -0,0 +1,10 @@
+﻿// Finds a first position of the element in the list
+let findPosition list value =
+    let rec loop list countdown =
+       match list with
+       | head :: tail when head = value -> Some(list.Length - countdown)
+       | head :: tail -> loop tail (countdown - 1)
+       | [] -> None
+    loop list 0
+
+printfn "%A" (findPosition [ 1..10000 ] 9999)
