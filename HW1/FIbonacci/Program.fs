d8598019a1eca74c32353ad5578723fd17f3b44a
Alexander-Ploskin <ploskin0107@gmail.com>
2021-02-14 15:27:15 +0300

    made an Fibonacci calculator

---
+++ HW1/FIbonacci/Program.fs
@@ -0,0 +1,9 @@
+﻿// Calculates the Fibonacci number by an index
+let fibonacci number =
+    if (number < 0) then raise(System.ArgumentException("number should not be negative"))
+    let rec loop first second countdown =
+        if (countdown = 0) then first
+        else loop second (first + second) (countdown - 1)
+    loop 0 1 number
+    
+printfn "%A" (fibonacci 10)
