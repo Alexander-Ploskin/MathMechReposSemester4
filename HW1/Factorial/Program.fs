17bd045410c455f23b4fc062e868a5b1d0e2cc2c
Alexander-Ploskin <ploskin0107@gmail.com>
2021-02-14 14:33:04 +0300

    made a factorial function

---
+++ HW1/Factorial/Program.fs
@@ -0,0 +1,7 @@
+﻿/// Calculates factorial of a number
+let rec factorial number =
+    if (number < 0) then raise(System.ArgumentException("number should not be negative"))
+    elif (number < 2) then 1
+    else number * factorial (number - 1)
+
+printfn "%A" (factorial 5) 
