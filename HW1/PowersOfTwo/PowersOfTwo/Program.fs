39a92e7c28b7bf695dbbe91bdd57ee99e54d246d
Alexander-Ploskin <ploskin0107@gmail.com>
2021-02-16 09:37:10 +0300

    Fixed function according to task

--- HW1/PowersOfTwo/PowersOfTwo/Program.fs
+++ HW1/PowersOfTwo/PowersOfTwo/Program.fs
@@ -1,12 +1,11 @@
-﻿// Creates list of all powers of five from n to m
+﻿// Creates list of all powers of two from n to n + m
 let getPowersOfTwo n m =
     let result = [1]
     if (m < 0 or n < 0) then raise(System.ArgumentException("arguments should not be negative"))
-    if (m < n) then raise(System.ArgumentException("lower border of the interval should be less than upper border"))
     let rec loop acc countdown =
         if (countdown = 0) then acc
         elif (countdown <= n) then loop (List.map (fun x -> x * 2) acc) (countdown - 1)
         else loop ((acc.Head * 2) :: acc) (countdown - 1)
-    List.fold (fun acc elem -> elem::acc) [] (loop result m)
+    List.rev (loop result (m + n))
     
-printfn "%A" (getPowersOfTwo 5 20)
+printfn "%A" (getPowersOfTwo 7 4)
