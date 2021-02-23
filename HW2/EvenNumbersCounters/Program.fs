66954e4d9961fbab97fa2242b18873e7cc124dd0
Alexander-Ploskin <ploskin0107@gmail.com>
2021-02-23 18:16:46 +0300

    Made counters

---
+++ HW2/EvenNumbersCounters/Program.fs
@@ -0,0 +1,12 @@
+﻿namespace EvenNumbersCounters
+
+//Contains functions that counts amount of even numbers in list
+module Counters =
+    let mapEvenNumbersCounter list =
+        List.map (fun number -> (abs(number) + 1) % 2) list |> List.sum
+
+    let filterEvenNumbersCounter list = 
+        List.filter (fun number -> number % 2 = 0) list |> List.length
+
+    let foldEvenNumbersCounter list = 
+        List.fold (fun acc number -> acc + (abs(number) + 1) % 2) 0 list
