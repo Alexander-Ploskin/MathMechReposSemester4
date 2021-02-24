1efaef75d4116209b955abe5db0c3b25e4068e86
Alexander-Ploskin <ploskin0107@gmail.com>
2021-02-24 15:35:20 +0300

    Made map for trees

---
+++ HW2/MapForTrees/MapForTrees.fs
@@ -0,0 +1,12 @@
+﻿module MapForTrees
+
+// Binary tree
+type Tree<'a> =
+    | Empty
+    | Tree of 'a * Tree<'a> * Tree<'a>
+
+// Returns new tree with values given by the function on values in nodes of the tree
+let rec treeMap tree func = 
+    match tree with 
+    | Empty -> Empty
+    | Tree(value, left, right) -> Tree(func value, treeMap left func, treeMap right func)
---
+++ HW2/MapForTreesTests/MapForTreesTests.fs
@@ -0,0 +1,45 @@
+module MapForTreesTests
+
+open NUnit.Framework
+open FsCheck
+open FsUnit
+open MapForTrees
+
+let rec areEqual tree1 tree2 =
+    match tree1, tree2 with
+    | Empty, Empty -> true
+    | Tree(value1, left1, right1), Tree(value2, left2, right2) -> value1 = value2 && (areEqual left1 left2) && (areEqual right1 right2)
+    | _ -> false
+
+let rec haveSameStructure tree1 tree2 =
+    match tree1, tree2 with
+    | Empty, Empty -> true
+    | Tree(value1, left1, right1), Tree(value2, left2, right2) -> (haveSameStructure left1 left2) && (haveSameStructure right1 right2)
+    | _ -> false
+
+[<Test>]
+let ``Should work with empty tree``() =
+    areEqual (treeMap Empty (fun x -> x)) Empty |> should be True
+
+[<Test>]
+let ``Should work with leaf``() =
+    areEqual (treeMap (Tree(2, Empty, Empty)) (fun x -> x * x)) (Tree(4, Empty, Empty)) |> should be True
+
+[<Test>]
+let ``Should work with children``() =
+    let tree = Tree(2, (Tree(3, Empty, Empty)), (Tree(4, Empty, Empty)))
+    let squaredTree = Tree(4, (Tree(9, Empty, Empty)), (Tree(16, Empty, Empty)))
+    areEqual (treeMap tree (fun x -> x * x)) squaredTree |> should be True
+
+[<Test>]
+let ``Should work with grandchildren``() =
+    let tree = Tree(2, (Tree(3, (Tree(4, Empty, Empty)), (Tree(5, Empty, Empty)))), (Tree(6, (Tree(7, Empty, Empty)), (Tree(8, Empty, Empty)))))
+    let squaredTree = Tree(4, (Tree(9, (Tree(16, Empty, Empty)), (Tree(25, Empty, Empty)))), (Tree(36, (Tree(49, Empty, Empty)), (Tree(64, Empty, Empty)))))
+    areEqual (treeMap tree (fun x -> x * x)) squaredTree |> should be True
+
+[<Test>]
+let ``Should keep the structure``() =
+    let checkIfKeepStructure tree =
+        let mappedTree = treeMap tree (fun x -> x)
+        haveSameStructure tree mappedTree
+    Check.QuickThrowOnFailure checkIfKeepStructure
