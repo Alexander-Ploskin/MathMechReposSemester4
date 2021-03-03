cbb0562d8c9dc539a43efc759c382f4687b5669c
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-03 11:49:32 +0300

    Added comments

--- HW2/SequenceOfPrimes/SequenceOfPrimes/Program.fs
+++ HW2/SequenceOfPrimes/SequenceOfPrimes/Program.fs
@@ -1,12 +1,17 @@
 ﻿module SequenceOfPrimes
 
-// Checks if number is prime
+/// <summary>
+/// Checks if number is prime
+/// </summary
+/// <param name="number">Number to check</param>
 let isPrime (number : bigint) =
     seq { bigint 2 .. bigint(System.Math.Sqrt(float number))}
     |> Seq.exists (fun x -> number % x = bigint 0)
     |> not
 
-// Creates infinity sequence of prime numbers
+/// <summary>
+/// // Creates infinity sequence of prime numbers
+/// </summary
 let primes () =
     Seq.initInfinite (fun i -> bigint i + bigint 2)
     |> Seq.filter isPrime
