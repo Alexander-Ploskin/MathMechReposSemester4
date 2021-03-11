d015965c2766f52c0baf956d800d9bed28113f4f
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-11 10:01:56 +0300

    one more attempt

--- HW3/LambdaInterpreter/LambdaInterpreterTests/LambdaInterpreterTests.fs
+++ HW3/LambdaInterpreter/LambdaInterpreterTests/LambdaInterpreterTests.fs
@@ -14,10 +14,10 @@ let K = Abstraction(x, Abstraction(y, Variable x))
 let K_Star = Abstraction(x, Abstraction(y, Variable y))
 
 let SimpleExamples = [|
-    (Application(I, I)), I
-    (Application(K, I)), K_Star
-    (Application(K, Application(K_Star, K))), K
-    (Application(K_Star, Application(I, I))), I
+    ((Application(I, I)), I)
+    ((Application(K, I)), K_Star)
+    ((Application(K, Application(K_Star, K))), K)
+    ((Application(K_Star, Application(I, I))), I)
 |]
 
 [<TestCaseSource(nameof(SimpleExamples))>]
