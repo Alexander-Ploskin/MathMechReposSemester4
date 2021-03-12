f58388a6f74d81fe6b07b78912d24ca523d2a34c
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-12 16:39:22 +0300

    Add missed file

---
+++ HW3/LambdaInterpreter/LambdaInterpreterTests/HelpFunctionsTests.fs
@@ -0,0 +1,58 @@
+﻿module HelpersTests
+
+open System
+open NUnit.Framework
+open FsUnit
+open FsCheck
+
+open LambdaInterpreter
+
+[<TestFixture>]
+type HelpersTests() =
+    [<Test>]
+    member this.``New variables generator should work correct``() =
+        let CheckIfNotContainedInSet set =
+            set |> Set.contains (getNewValueNotFromTheSet set) |> not
+        Check.QuickThrowOnFailure CheckIfNotContainedInSet
+
+    static member GetFreeVariablesCases =
+        let x = Guid.NewGuid()
+        let y = Guid.NewGuid()
+        let z = Guid.NewGuid()
+        [|
+            Application(Variable x, Abstraction(x, Application(Variable y, Variable z))), set [x; y; z]
+            Application(Abstraction(x, Variable x), Abstraction(y, Variable y)), Set.empty
+        |]
+
+    static member SubstitutionCasesWithoutNameConflicts =
+        let x = Guid.NewGuid()
+        let y = Guid.NewGuid()
+        let z = Guid.NewGuid()
+        [|
+            (Variable x), y, (Application(Variable y, Variable z)), Variable x
+            (Application(Variable x, Variable x)), x, (Variable z), Application(Variable z, Variable z)
+            (Abstraction(x, Variable x)), x, (Application(Variable y, Variable z)), Abstraction(x, Variable x)
+            (Abstraction(y, Variable x)), x, (Variable z), (Abstraction(y, Variable z))
+        |]
+
+    [<TestCaseSource(nameof(HelpersTests.GetFreeVariablesCases))>]
+    member this.``Should get free variables correctly``(testCase) =
+        let term, expected = testCase
+        term |> getFreeVariables |> should equal expected
+    
+    [<TestCaseSource(nameof(HelpersTests.SubstitutionCasesWithoutNameConflicts))>]
+    member this.``Should substitute variables correctly``(testCase) =
+        let term, varToSubstitute, termToSubstitute, expected = testCase
+        term |> substitute varToSubstitute termToSubstitute |> should equal expected
+
+    [<Test>]
+    member this.``Substitution with name collision should be successful`` () =
+        let x = Guid.NewGuid()
+        let y = Guid.NewGuid()
+        let result = substitute (Abstraction(y, Variable x)) x (Variable y)
+        match result with
+        | Abstraction (_, term) -> 
+            match term with
+            | Variable name -> name |> should equal y
+            | _ -> Assert.Fail()
+        | _ -> Assert.Fail()
