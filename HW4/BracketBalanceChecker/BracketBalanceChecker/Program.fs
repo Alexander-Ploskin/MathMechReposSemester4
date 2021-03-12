4baf134b4f1148ecf3517693984297e3fee979c6
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-12 14:36:04 +0300

    Write tests

--- HW4/BracketBalanceChecker/BracketBalanceChecker/Program.fs
+++ HW4/BracketBalanceChecker/BracketBalanceChecker/Program.fs
@@ -1,21 +1,24 @@
 ﻿module BracketBalanceChecker
 
+/// Defines current state of loop
 type BalanceState =
     { Square: int;
       Round: int;
       Curly: int; }
 
-let faf = "fwefwafw3efr"
-Seq.fold id "" 
-
-let checkBracketBalance string =
-    let rec loop (string : string) state =
-        if state.Curly < 0 || state.Round < 0 || state.Square < 0 then false
-        else match string with 
-             | head :: tail when head = '[' -> loop tail (state.Square + 1)
-             | head :: tail when head = ']' -> loop tail (state.Square - 1)
-             | head :: tail when head = '(' -> loop tail (state.Square + 1)
-             | head :: tail when head = ')' -> loop tail (state.Square - 1)
-             | head :: tail when head = '{' -> loop tail (state.Square + 1)
-             | head :: tail when head = '}' -> loop tail (state.Square - 1)
-    loop string BalanceState {Square = 0; Round =  0; Curly = 0}
+/// Checks if string is balanced
+let isBalanced string =
+  let rec loop state = function
+    | ']'::_  when state.Square = 0 -> false
+    | ']'::tail -> loop ({Square = state.Square - 1; Round = state.Round; Curly = state.Curly}) tail
+    | '['::tail -> loop ({Square = state.Square + 1; Round = state.Round; Curly = state.Curly}) tail
+    | ')'::_  when state.Round = 0 -> false
+    | ')'::tail -> loop ({Square = state.Square; Round = state.Round - 1; Curly = state.Curly}) tail
+    | '('::tail -> loop ({Square = state.Square; Round = state.Round + 1; Curly = state.Curly}) tail
+    | '}'::_  when state.Curly = 0 -> false
+    | '}'::tail -> loop ({Square = state.Square; Round = state.Round; Curly = state.Curly - 1}) tail
+    | '{'::tail -> loop ({Square = state.Square; Round = state.Round; Curly = state.Curly + 1}) tail
+    | [] -> state.Round = 0 && state.Square = 0 && state.Curly = 0
+    | _::tail -> loop state tail
+ 
+  string |> Seq.toList |> loop {Square = 0; Round = 0; Curly = 0}
---
+++ HW4/BracketBalanceChecker/BracketBalanceCheckerTests/BalanceCheckerTests.fs
@@ -0,0 +1,43 @@
+module BracketBalanceCheckerTests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+
+open BracketBalanceChecker
+
+[<TestFixture>]
+type BracketBalanceChecherTest() =
+    static member testCases() = [|
+        ("", true)
+        (")", false)
+        ("]", false)
+        ("}", false)
+        ("(", false)
+        ("[", false)
+        ("{", false)
+        ("()", true)
+        ("{}", true)
+        ("[]", true)
+        ("())", false)
+        ("[]]", false)
+        ("{}}", false)
+        ("())(", false)
+        ("[]][", false)
+        ("{}}{", false)
+        ("F)", false)
+        ("(F)", true)
+        ("()F", true)
+        (" )", false)
+        ("( )", true)
+        ("() ", true)
+        ("(]", false)
+        ("[)", false)
+        ("{]", false)
+    |]
+
+    [<Test>]
+    [<TestCaseSource("testCases")>]
+    member this.``Should work correct on basic examples`` (testCase: string * bool) =
+        let string, expected = testCase
+        string |> isBalanced |> should equal expected
