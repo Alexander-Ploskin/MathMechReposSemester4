2bfa7b6235436e652741955afdf199f657566fd7
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-16 22:16:18 +0300

    Take out data manipulating functions

--- HW4/Phonebook/Phonebook/Program.fs
+++ HW4/Phonebook/Phonebook/Program.fs
@@ -4,6 +4,8 @@ open System
 open System.Text.RegularExpressions
 open System.IO
 
+open Data
+
 /// Prints introduction which contains all necessary commands
 let printIntroduction =
     printfn "Welcome to the phonebook app"
@@ -16,21 +18,6 @@ let printIntroduction =
     printfn "5 - print all notes"
     printfn "6 - save data"
     printfn "7 - get data from file"
-
-/// Regular expression which matches with russian phone numbers
-let numberValidationExpression =
-    "^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"
-
-/// Regular expression which matches with .txt files
-let pathValidationExpression =
-    "^(?:[\w]\:|\\)(\\[a-z_\-\s0-9\.]+)+\.txt$"
-
-/// Wrapper of Regex function, returns true if string matches with expression
-let matchWithExpression string expression =
-    try
-        Regex.IsMatch(string, expression)
-    with
-        | :? ArgumentException -> false
 
 /// Gets console input of string which should be correct by defenition in isCorrect function
 let rec enterCorrect isCorrect =
@@ -63,36 +50,34 @@ let findNumbers notes =
 let findOwners notes =
     printfn "Enter the number:"
     let number = enterCorrect (matchWithExpression numberValidationExpression)
-    let searchResult = Set.filter (fun note -> snd note = number) notes
     printfn $"Owners of {number}:"
-    Set.iter (fun note -> printfn "%A" <| fst note) searchResult
+    Set.iter (fun note -> printfn "%A" <| note) (selectOwners notes number)
 
 /// Saves current data into file by the specied path
 let saveData notes =
-    printfn "Enter the database file path or path to create new file"
+    printfn "Enter the database file path or path to create new file. File should be .txt"
     let path = enterCorrect (matchWithExpression pathValidationExpression)
-    let bigNote = Set.map (fun note -> $"{fst note}-{snd note}") notes
     try
-        File.WriteAllLines(path, bigNote)
+        writeNotesIntoFile notes path
         printfn "Saved"
     with
-        | :? FileNotFoundException -> printfn "There's not file by this path"
-        | :? ArgumentException -> printfn "Path shouldn't be null"
-        | :? UnauthorizedAccessException -> printfn "Access denied"
+        | Failure(message) -> printfn "%s" message
 
 /// Reads data from the file
 let readData notes =
-    printfn "Enter the database file path"
+    printfn "Enter the database file path. File should be .txt"
     let path = enterCorrect (matchWithExpression pathValidationExpression)
     try
-        let temp = Seq.map (fun (string : String) -> (Array.get (string.Split("-")) 0, Array.get (string.Split("-")) 1)) (File.ReadAllLines(path))
+        let result = readNotesFromFile notes path
         printfn "Read"
-        Set.ofSeq temp
-    with 
-        | :? FileNotFoundException  -> printfn "There is not file by this path"
-                                       notes
-        | :? ArgumentException -> printfn "Path shouldn't be null"
-                                  notes
+        result
+    with
+        | Failure(message) -> printfn "%s" message
+                              notes
+
+/// Prints all notes on console
+let printData notes =
+    Set.iter (fun note -> printfn "%A" <| $"{fst note} - {snd note}") notes
 
 /// Main function of the application
 let main =
@@ -105,7 +90,7 @@ let main =
                  loop notes 
         | "4" -> findOwners notes
                  loop notes 
-        | "5" -> Set.iter (fun note -> printfn "%A" <| $"{fst note} - {snd note}") notes
+        | "5" -> printData notes
                  loop notes 
         | "6" -> saveData notes
                  loop notes
