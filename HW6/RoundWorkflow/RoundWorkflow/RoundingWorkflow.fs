b8a0fe2131fae729ea4d62a6dde8ffcee02f8c19
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-01 17:30:52 +0300

    Create rounding workflow

---
+++ HW6/RoundWorkflow/RoundWorkflow/RoundingWorkflow.fs
@@ -0,0 +1,10 @@
+﻿module RoundingWorkflow
+
+open System
+
+// Workflow which provides rounding calculation with a specified accuracy
+type RoundingCounter(accuracy : int) =
+    member this.Bind (x : float, f) =
+        f (Math.Round(x, accuracy))
+    member this.Return(x : float) =
+        Math.Round(x, accuracy)
