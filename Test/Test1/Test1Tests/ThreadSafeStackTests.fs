d817e0370a3b18d82bac18c2c88bb8fd0919898a
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-02 18:41:32 +0300

    remove unnecessary spaces

--- Test/Test1/Test1Tests/ThreadSafeStackTests.fs
+++ Test/Test1/Test1Tests/ThreadSafeStackTests.fs
@@ -9,8 +9,15 @@ let ``Should return none if stack is empty`` () =
     Stack<int>().TryPop() |> should equal None
 
 [<Test>]
-let ``Should correct push value in one thread`` () =
+let ``Should correct push one value in one thread`` () =
     let stack = Stack<string>()
     stack.Push("A")
     stack.TryPop() |> should equal <| Some("A")
 
+[<Test>]
+let ``Should work correctly with many values`` () =
+    let stack = Stack<int>()
+    stack.Push(1)
+    stack.Push(0)
+    stack.TryPop() |> should equal <| Some("0")
+    stack.TryPop() |> should equal <| Some("1")
