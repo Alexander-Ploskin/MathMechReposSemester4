a9c99aff8e1cdb06cf9a758dc03fa3f6600f3e6d
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-02 18:27:18 +0300

    Add single thread tests

---
+++ Test/Test1/Test1Tests/ThreadSafeStackTests.fs
@@ -0,0 +1,16 @@
+﻿module ThreadSafeStackTests
+
+open FsUnit
+open NUnit.Framework
+open ThreadSafeStack
+
+[<Test>]
+let ``Should return none if stack is empty`` () =
+    Stack<int>().TryPop() |> should equal None
+
+[<Test>]
+let ``Should correct push value in one thread`` () =
+    let stack = Stack<string>()
+    stack.Push("A")
+    stack.TryPop() |> should equal <| Some("A")
+
