b7fd5c9680dc95d0157aed1b1e1286c18928935e
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-09 16:49:52 +0300

    Add single threaded lazy

---
+++ HW7/Lazy/Lazy/Lazy/SingleThreadedLazy.fs
@@ -0,0 +1,14 @@
+﻿module SingleThreadedLazy
+
+open ILazy
+
+type SingleThreadedLazy<'a> (supplier : unit -> 'a) =
+    let mutable calculated = false
+    let mutable result = None
+
+    interface ILazy<'a> with
+        member this.Get() =
+            if calculated |> not then
+                calculated <- true
+                result <- Some(supplier())
+            result.Value
