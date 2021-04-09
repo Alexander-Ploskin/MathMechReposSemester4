bfe0a60eeda42ecb41e5bf2486f7a684e9c467dd
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-09 22:47:31 +0300

    Write single threaded creator in lazy factory

---
+++ HW7/Lazy/Lazy/Lazy/LazyFactory.fs
@@ -0,0 +1,8 @@
+﻿module LazyFactory
+
+open SingleThreadedLazy
+open ILazy
+
+type LazyFactory =
+    static member CreateSingleThreadedLazy supplier =
+        new SingleThreadedLazy<'a>(supplier) :> ILazy<'a>
