d7234247eb1100fc1d6443912dfc29f0b07e8d4a
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-11 13:59:06 +0300

    Write lock-free lazy

---
+++ HW7/Lazy/Lazy/Lazy/LockFreeLazy.fs
@@ -0,0 +1,17 @@
+﻿module LockFreeLazy
+
+open ILazy
+open System.Threading
+
+type SingleThreadedLazy<'a> (supplier : unit -> 'a) =
+    let mutable calculated = false
+    let mutable result = None
+
+    interface ILazy<'a> with
+        member this.Get() =
+            if calculated |> not then
+                let startValue = result
+                let desiredValue = Some <| supplier()
+                Interlocked.CompareExchange(&result, desiredValue, startValue) |> ignore
+                calculated <- true
+            result.Value
