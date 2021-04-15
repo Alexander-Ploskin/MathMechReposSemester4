da78d7ad7ca222caa359cd6b032d7f4e7672d416
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-16 00:35:50 +0300

    Rewrite point free function to be working

--- HW4/PointFree/PointFreeTests/PointFreeTests.fs
+++ HW4/PointFree/PointFreeTests/PointFreeTests.fs
@@ -7,4 +7,4 @@ open PointFree
 
 [<Test>]
 let ``Should have same output with initial function`` () =
-    Check.Quick (fun number list -> (multiplyByNumber number list) = (multiplyByNumber'3 number list))
+    Check.QuickThrowOnFailure (fun number list -> (multiplyByNumber number list) = (multiplyByNumber'3() number list))
