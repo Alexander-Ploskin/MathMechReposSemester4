0fcc811130cccad69267c9a04acf72163073e198
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-17 19:18:48 +0300

    add necessary space

--- HW4/PointFree/PointFree/Program.fs
+++ HW4/PointFree/PointFree/Program.fs
@@ -10,4 +10,4 @@ let multiplyByNumber'1 x = List.map (fun y -> y * x)
 let multiplyByNumber'2 x = List.map ((*) x)
 
 /// Eta-converted inner function
-let multiplyByNumber'3() = (*) >> List.map
+let multiplyByNumber'3 () = (*) >> List.map
