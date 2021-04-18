3a60a40cac56d2ef635229bfb141ce1e7bc1679b
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-18 10:56:31 +0300

    Simplify main function

--- HW7/MiniCrawler/MiniCrawler/Crawler.fs
+++ HW7/MiniCrawler/MiniCrawler/Crawler.fs
@@ -4,12 +4,12 @@ open System.IO
 open System.Net
 open System.Text.RegularExpressions
 
-// Finds all links matching with linkRegex
+/// Finds all links matching with linkRegex
 let getAllLinks (html : string) =
     [for matches in (Regex("<a href\s*=\s*\"?(https?://[^\"]+)\"?\s*>", RegexOptions.Compiled).Matches(html) : MatchCollection) 
     -> matches.Groups.[1].Value]
 
-// Downloads page by url asynchronously
+/// Downloads page by url asynchronously
 let fetchAsync (url : string) =
        async {
            try
@@ -18,39 +18,28 @@ let fetchAsync (url : string) =
                use stream = response.GetResponseStream()
                use reader = new StreamReader(stream)
                let html = reader.ReadToEnd()
-               do printfn "Downloaded page %s with size of %d" url html.Length
                return Some html
            with 
                | _ -> printfn "Can't reach the page"
                       return None
        }
 
-// Saves content into new file by path asynchronously
-let saveContentAsync (path : string) (content : string) =
-    async {
-        try
-            use stream = File.Create(path)
-            use writer = new StreamWriter(stream)
-            return writer.Write(content)
-        with
-        | :?IOException -> failwith "Invalid path"
-    }
+/// Prints result of downloading
+let printResult url (downloaded : Option<string>) = 
+    printfn "Pages linked to this page"
+    match downloaded with
+    | Some html -> printfn "%s    Length: %d symbols" url html.Length
+    | _ -> printfn "Bad response for %s" url
 
-// Gets function of saving page getten in a form of option asynchronously
-let savePageAsync (path : string) (content : Option<_>) =
-    match content with
-    | Some html -> saveContentAsync path html
-    | _ -> async { return() }
-
-// Downloads all pages into the path which linked in the page by the url
-let downloadLinkedPages (url : string) (path : string) =
+/// Downloads all pages into the path which linked in the page by the url
+let downloadLinkedPages (url : string) =
     let mainPageContent = fetchAsync url |> Async.RunSynchronously
     match mainPageContent with
-    | Some content -> Directory.CreateDirectory(path) |> ignore
-                      let links = content |> getAllLinks
+    | Some content -> let links = content |> getAllLinks
                       let downloaded = links |> List.map (fun link -> link |> fetchAsync) |> Async.Parallel |> Async.RunSynchronously
-                      downloaded |> Array.mapi (fun i option -> savePageAsync $"{path}/{i}.html" option) |> Async.Parallel |> Async.RunSynchronously |> ignore
+                      downloaded |>  Array.iteri (fun i (result : string option) -> printResult (links.Item i) result)
+                      
     | None -> ()
 
 // Example of usage
-downloadLinkedPages "https://github.com/Alexander-Ploskin" "GitHub.html"
+downloadLinkedPages "https://github.com/Alexander-Ploskin"
